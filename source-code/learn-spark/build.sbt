1e1bd44831b488a28069e3bc9a35cd9eda1d5a06
Nadim Bahadoor <nbahadoor@gmail.com>
2017-07-13 23:45:59 +0100

    Project setup and data frame from csv example.

---
+++ source-code/learn-spark/build.sbt
@@ -0,0 +1,12 @@
+name := "learn-spark"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark"  %%  "spark-core"    % "2.2.0"   % "provided",
+  "org.apache.spark"  %%  "spark-sql"     % "2.2.0",
+  "org.apache.spark"  %%  "spark-mllib"   % "2.2.0"
+)
+    
---
+++ source-code/learn-spark/src/main/scala/com/allaboutscala/learn/spark/utils/Context.scala
@@ -0,0 +1,38 @@
+package com.allaboutscala.learn.spark.utils
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+/**
+  * Created by Nadim Bahadoor on 28/06/2016.
+  *
+  *  Tutorial: Learn How To Use Apache Spark
+  *
+  * [[http://allaboutscala.com/big-data/spark/]]
+  *
+  * Copyright 2016 Nadim Bahadoor (http://allaboutscala.com)
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  * use this file except in compliance with the License. You may obtain a copy of
+  * the License at
+  *
+  *  [http://www.apache.org/licenses/LICENSE-2.0]
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+  * License for the specific language governing permissions and limitations under
+  * the License.
+  */
+trait Context {
+
+  lazy val sparkConf = new SparkConf()
+    .setAppName("Learn Spark")
+    .setMaster("local[*]")
+    .set("spark.cores.max", "2")
+
+  lazy val sparkSession = SparkSession
+    .builder()
+    .config(sparkConf)
+    .getOrCreate()
+}
