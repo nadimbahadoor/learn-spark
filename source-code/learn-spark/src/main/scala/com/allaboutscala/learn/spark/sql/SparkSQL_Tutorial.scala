c5d36ce82eb504d209e8f382476079951ba2c015
Nadim Bahadoor <nbahadoor@gmail.com>
2017-07-24 22:39:27 +0100

    Added Spark SQL examples: typed columns, filter, create temporary tables, SQL inner join, left outer join, right outer join.

--- source-code/learn-spark/src/main/scala/com/allaboutscala/learn/spark/sql/SparkSQL_Tutorial.scala
+++ source-code/learn-spark/src/main/scala/com/allaboutscala/learn/spark/sql/SparkSQL_Tutorial.scala
@@ -1,5 +1,6 @@
 package com.allaboutscala.learn.spark.sql
 
+import com.allaboutscala.learn.spark.dataframe.DataFrame_Tutorial._
 import com.allaboutscala.learn.spark.utils.Context
 
 /**
@@ -127,6 +128,87 @@ object SparkSQL_Tutorial extends App with Context {
         |from so_tags group by tag having count > 5 order by tag""".stripMargin)
     .show(10)
 //  dfTags.groupBy("tag").count().filter("count > 5").orderBy("tag").show(10)
+
+
+
+  // Typed dataframe, filter and temp table
+  val dfQuestionsCSV = sparkSession
+    .read
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .option("dateFormat","yyyy-MM-dd HH:mm:ss")
+    .csv("src/main/resources/questions_10K.csv")
+    .toDF("id", "creation_date", "closed_date", "deletion_date", "score", "owner_userid", "answer_count")
+
+
+  // cast columns to data types
+  val dfQuestions = dfQuestionsCSV.select(
+    dfQuestionsCSV.col("id").cast("integer"),
+    dfQuestionsCSV.col("creation_date").cast("timestamp"),
+    dfQuestionsCSV.col("closed_date").cast("timestamp"),
+    dfQuestionsCSV.col("deletion_date").cast("date"),
+    dfQuestionsCSV.col("score").cast("integer"),
+    dfQuestionsCSV.col("owner_userid").cast("integer"),
+    dfQuestionsCSV.col("answer_count").cast("integer")
+  )
+
+  // filter dataframe
+  val dfQuestionsSubset = dfQuestions.filter("score > 400 and score < 410").toDF()
+
+  // register temp table
+  dfQuestionsSubset.createOrReplaceTempView("so_questions")
+
+
+  // SQL Inner Join
+  sparkSession
+    .sql(
+    """
+      |select t.*, q.*
+      |from so_questions q
+      |inner join so_tags t
+      |on t.id = q.id
+    """.stripMargin)
+    .show(10)
+
+  // DataFrame Query: Inner Join
+//  dfQuestionsSubset
+//    .join(dfTags, Seq("id"), "inner")
+//    .show(10)
+
+
+  // SQL Left Outer Join
+  sparkSession
+    .sql(
+      """
+        |select t.*, q.*
+        |from so_questions q
+        |left outer join so_tags t
+        |on t.id = q.id
+      """.stripMargin)
+    .show(10)
+
+//  dfQuestionsSubset
+//    .join(dfTags, Seq("id"), "left_outer")
+//    .show(10)
+
+
+
+  // SQL Right Outer Join
+  sparkSession
+    .sql(
+      """
+        |select t.*, q.*
+        |from so_tags t
+        |right outer join so_questions q
+        |on t.id = q.id
+      """.stripMargin)
+    .show(10)
+
+//  dfTags
+//    .join(dfQuestionsSubset, Seq("id"), "right_outer")
+//    .show(10)
+
+
 
   sparkSession.stop()
 }
