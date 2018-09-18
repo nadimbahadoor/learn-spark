add3fbe5e91ffb31878f3ced81ca6a1eb7061d52
Nadim Bahadoor <nbahadoor@gmail.com>
2018-09-18 16:23:31 +0400

    Spark Functions: Search DataFrame column using array_contains() method.

---
+++ source-code/learn-spark/src/main/scala/com/allaboutscala/learn/spark/functions/Tutorial_06_DataFrameColumnArrayContains.scala
@@ -0,0 +1,52 @@
+package com.allaboutscala.learn.spark.functions
+
+import com.allaboutscala.learn.spark.utils.Context
+import org.apache.spark.sql.functions._
+
+/**
+  * Created by Nadim Bahadoor on 28/06/2016.
+  *
+  *  Tutorial: Learn How To Use Apache Spark
+  *
+  * [[http://allaboutscala.com/big-data/spark/]]
+  *
+  * Copyright 2016 Nadim Bahadoor (http://allaboutscala.com)
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  * use this file except in compliance with the License. You may obtain a copy of
+  * the License at
+  *
+  *  [http://www.apache.org/licenses/LICENSE-2.0]
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+  * License for the specific language governing permissions and limitations under
+  * the License.
+  */
+object Tutorial_06_DataFrameColumnArrayContains extends App with Context {
+
+  import sparkSession.sqlContext.implicits._
+
+  val tagsDF = sparkSession
+    .read
+    .option("multiLine", true)
+    .option("inferSchema", true)
+    .json("src/main/resources/tags_sample.json")
+
+  val df = tagsDF
+    .select(explode($"stackoverflow") as "stackoverflow_tags")
+    .select(
+      $"stackoverflow_tags.tag.id" as "id",
+      $"stackoverflow_tags.tag.author" as "author",
+      $"stackoverflow_tags.tag.name" as "tag_name",
+      $"stackoverflow_tags.tag.frameworks.id" as "frameworks_id",
+      $"stackoverflow_tags.tag.frameworks.name" as "frameworks_name"
+    )
+  df.show()
+
+  df
+    .select("*")
+    .where(array_contains($"frameworks_name","Play Framework"))
+    .show()
+}
