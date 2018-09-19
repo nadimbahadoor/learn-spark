fbb9fbb192e8f60192315cd70ec9b6c5d72711bd
Nadim Bahadoor <nbahadoor@gmail.com>
2018-09-19 11:54:56 +0400

    Spark Functions: Check DataFrame column exists

---
+++ source-code/learn-spark/src/main/scala/com/allaboutscala/learn/spark/functions/Tutorial_07_CheckDataFrameColumnExist.scala
@@ -0,0 +1,35 @@
+package com.allaboutscala.learn.spark.functions
+
+import com.allaboutscala.learn.spark.utils.Context
+
+/**
+  * Created by Nadim Bahadoor on 28/06/2016.
+  *
+  *  Tutorial: Learn How To Use Apache Spark
+  *
+  * [[http://allaboutscala.com/big-data/spark/]]
+  *
+  * Copyright 2016 Nadim Bahadoor (http://allaboutscala.com)
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  * use this file except in compliance with the License. You may obtain a copy of
+  * the License at
+  *
+  *  [http://www.apache.org/licenses/LICENSE-2.0]
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+  * License for the specific language governing permissions and limitations under
+  * the License.
+  */
+object Tutorial_07_CheckDataFrameColumnExist extends App with Context {
+
+  val donuts = Seq(("plain donut", 1.50), ("vanilla donut", 2.0), ("glazed donut", 2.50))
+  val df = sparkSession.createDataFrame(donuts).toDF("Donut Name", "Price")
+
+  df.show()
+
+  val priceColumnExists = df.columns.contains("Price")
+  println(s"Does price column exist = $priceColumnExists")
+}
