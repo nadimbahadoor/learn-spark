c1831a17db684ead0b5bd2006eba429627b837b1
Nadim Bahadoor <nbahadoor@gmail.com>
2018-10-01 16:45:55 +0400

    Spark Functions: Format DataFrame column

--- source-code/learn-spark/src/main/scala/com/allaboutscala/learn/spark/functions/Tutorial_12_Formatting_DataFrame_Column.scala
+++ source-code/learn-spark/src/main/scala/com/allaboutscala/learn/spark/functions/Tutorial_12_Formatting_DataFrame_Column.scala
@@ -23,20 +23,26 @@ import com.allaboutscala.learn.spark.utils.Context
   * License for the specific language governing permissions and limitations under
   * the License.
   */
-object Tutorial_11_DataFrameFirstRow extends App with Context {
+object Tutorial_12_Formatting_DataFrame_Column extends App with Context {
 
-  val donuts = Seq(("plain donut", 1.50), ("vanilla donut", 2.0), ("glazed donut", 2.50))
+
+  val donuts = Seq(("plain donut", 1.50, "2018-04-17"), ("vanilla donut", 2.0, "2018-04-01"), ("glazed donut", 2.50, "2018-04-02"))
   val df = sparkSession
     .createDataFrame(donuts)
-    .toDF("Donut Name", "Price")
-
-  val firstRow = df.first()
-  println(s"First row = $firstRow")
-
-  val firstRowColumn1 = df.first().get(0)
-  println(s"First row column 1 = $firstRowColumn1")
+    .toDF("Donut Name", "Price", "Purchase Date")
 
 
-  val firstRowColumnPrice = df.first().getAs[Double]("Price")
-  println(s"First row column Price = $firstRowColumnPrice")
+  import org.apache.spark.sql.functions._
+  import sparkSession.sqlContext.implicits._
+
+  df
+    .withColumn("Price Formatted", format_number($"Price", 2))
+    .withColumn("Name Formatted", format_string("awesome %s", $"Donut Name"))
+    .withColumn("Name Uppercase", upper($"Donut Name"))
+    .withColumn("Name Lowercase", lower($"Donut Name"))
+    .withColumn("Date Formatted", date_format($"Purchase Date", "yyyyMMdd"))
+    .withColumn("Day", dayofmonth($"Purchase Date"))
+    .withColumn("Month", month($"Purchase Date"))
+    .withColumn("Year", year($"Purchase Date"))
+    .show()
 }
